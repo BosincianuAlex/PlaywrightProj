14cc60605d4f354e48278e05af1f9349f27dddcf
BosincianuAlex <bos.roberto@ymail.com>
2025-04-25 17:07:29 +0300

    change

--- newTests/newTest1.spec.js
+++ newTests/newTest1.spec.js
@@ -4,31 +4,35 @@ const dataset = JSON.parse(JSON.stringify(require("../utils/TestData.json")))
 
 const url = "https:/www.emag.ro/";
 
-for(const data of dataset){
-    test(`test1 with ${data.product}`, async({page})=>{
-        const {MainPage, Filter, Catalogue} = new POManager(page)
-        await page.goto(url)
-        
-        await MainPage.searchBar.fill(`${data.product}`)
-        await MainPage.searchButton.click()            
 
-        await Filter.setFilter(data.memory)         
+test.beforeEach(async({page})=>{
+    await page.goto(url)
+    
+})
+
+test.describe("Filter test ", async(page) => {
+    for(const data of dataset){
+        test(`with ${data.product}`, async({page})=>{
+            const {MainPage, Filter, Catalogue} = new POManager(page)            
             
-        await page.waitForTimeout(1000)        
-          
-        await MainPage.listGridButton.click()  
-        
-        await page.waitForTimeout(1000)
-        
-        const count = await Catalogue.getItemsCount()
-        
-        let items = []
-        for(let i = 0 ; i < count; i++){            
-                       
-            let value= await Catalogue.items.nth(i).locator('p:has-text(\"memorie: \")').textContent()  
-            value = value.replace("Capacitate memorie:  ", "")
-            expect(value, "at: " + await Catalogue.items.nth(i).locator(".card-v2-title").textContent()).toBe(data.memory)
+            await MainPage.searchBar.fill(`${data.product}`)
+            await MainPage.searchButton.click()            
 
-        }      
+            await Filter.setFilter(data.memory)       
+            await page.waitForTimeout(1000)        
+            await MainPage.listGridButton.click()   
+            await page.waitForTimeout(1000)
+            
+            const count = await Catalogue.getItemsCount()
+            
+            let items = []
+            for(let i = 0 ; i < count; i++){            
+                        
+                let value= await Catalogue.items.nth(i).locator('p:has-text(\"Capacitate memorie: \")').textContent()  
+                value = value.replace("Capacitate memorie:  ", "")
+                expect(value, "at: " + await Catalogue.items.nth(i).locator(".card-v2-title").textContent()).toBe(data.memory)
 
-    })}
+            }    
+        })
+    }
+})    
--- pageobjects/Catalogue.js
+++ pageobjects/Catalogue.js
@@ -12,8 +12,7 @@ class Catalogue{
         
         let items = []
         for(let i = 0 ; i < count; i++){            
-
-            //console.log(await this.items.nth(i).locator(".card-v2-title").textContent())             
+                      
             let value= await this.items.nth(i).locator('p:has-text(\"Capacitate memorie: \")').textContent()  
             items.push(value.replace("Capacitate memorie:  ", ""))            
         }        
